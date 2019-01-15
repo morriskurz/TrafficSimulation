0765e6ccfe27012adf864392d18d0eabc15cfe37
Morris Kurz <morriskurz@gmail.com>
2019-01-15 12:45:15 +0100

    Change colormap, add flow analysis

--- TrafficPlot.m
+++ TrafficPlot.m
@@ -1,11 +1,9 @@
 A = dlmread('traffic.txt');
 [n, m] = size(A);
-average_vel = (sum(sum(A)) + sum(sum(A<0)))/(m*sum(A(1, :)>=0))
+average_vel = (sum(sum(A)) + sum(sum(A<0)))/(n*sum(A(1, :)>=0))
 %spy(A)
 %sum(A(149,:))
-cspy(A+1, 'ColorMap', 'gray', 'Level', 5)
-c = flipud(gray);
-colormap(c);
+cspy(A+1, 'ColorMap', 'winter', 'Level', 5)
 xlabel("x")
 ylabel("t")
 set(gca,'xtick',[0, m])
@@ -14,6 +12,8 @@ set(gca,'ytick',[0, n])
 set(gca,'yticklabel',[0, n])
 caxis([0 5]);
 axis tight
+set(gca,'FontSize',20,'fontWeight','bold')
+set(findall(gcf,'type','text'),'FontSize',20,'fontWeight','bold')
 
 %sum(A(13, :)) + sum(A(13, :) < 0)
 
--- main.cpp
+++ main.cpp
@@ -20,15 +20,22 @@ int main() {
     const int nrCells = 300;
     const double cellLength = 7.5;
     const double timeStepSec = 1;
-    const int timeSteps = 150;
+    const int timeSteps = 600;
     const double dallyFactor = 0.2;
+    const int amountOfSimulationRuns = 10000;
     std::ofstream outputFile;
     outputFile.open("traffic.txt");
-    int cells[nrCells*timeSteps];
+    int cells[nrCells*(timeSteps+1)];
+
+    int k = 8;
+    int checkpoint = std::max((int) (std::rand() % nrCells), k);
+    int carsCounted = 0;
+
     for (int i = 0; i < nrCells*timeSteps; i++){
         cells[i] = -1;
     }
 
+    std::cout << "Checkpoint at: " << checkpoint << "\n";
     std::cout << "Max velocity: " << maxCellsPerStep * cellLength / timeStepSec * 3.6 << ".\n";
     int count = 0;
     for (int i = 0; i < nrCells; i++){
@@ -36,18 +43,20 @@ int main() {
             count += 1;
             cells[i] = (std::rand() % (maxCellsPerStep + 1));
         }
-        outputFile << cells[i] << " ";
     }
+
+
     std::cout << "Initialized with " << count << " cars.\n";
-    for (int i = 1; i < timeSteps-1; i++){
-        outputFile << "\n";
+    for (int i = 1; i <= timeSteps; i++){
+        //  outputFile << "\n";
         for (int cellIndex = 0; cellIndex < nrCells; cellIndex++){
             int currentVel = cells[(i-1)*nrCells + cellIndex];
             if (currentVel >= 0){
                 // Accelerate
                 int distance = maxCellsPerStep;
                 for (int j = 1; j <= currentVel+1; j++){
-                    if (cells[(i-1)*nrCells + (cellIndex+j) % nrCells] >= 0){
+                    //std::cout << "Cell " << (i-1)*nrCells + cellIndex << ", inspecting " << (i-1)*nrCells + ((cellIndex+j) % nrCells) << "\n";
+                    if (cells[(i-1)*nrCells + ((cellIndex+j) % nrCells)] >= 0){
                         distance = j-1;
                         break;
                     }
@@ -60,9 +69,17 @@ int main() {
                     newVel = std::max(newVel - 1, 0);
                 }
                 // Move
-                cells[i*nrCells + ((cellIndex + newVel) % nrCells)] = newVel;
+                int newPos = ((cellIndex + newVel) % nrCells);
+                cells[i*nrCells + newPos] = newVel;
+                if (newPos > checkpoint and cellIndex < checkpoint){
+                    std::cout << "At time step " << i << ", a car passed through the checkpoint.\n";
+                    carsCounted++;
+                }
             }
             outputFile << cells[(i-1)*nrCells + cellIndex] << " ";
         }
+        outputFile << "\n";
     }
+    std::cout << "Cars counted: " << carsCounted << ", flow: " << carsCounted/(timeSteps*1.0)*3600 << " cars per hour.";
+    std::cout << "\nAverage Density: " << count/(cellLength*nrCells)*1000.0 << " cars per km.";
 }
